37f530bc2f7a28a0fef1d726ddbc82a46045d31b
João Victor <falejoaovictor@hotmail.com>
2024-01-29 17:08:07 -0300

    Added the arduino2 data

--- server/app.py
+++ server/app.py
@@ -5,19 +5,21 @@ import random
 import time
 
 app = Flask(__name__)
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser_arduino1 = serial.Serial('/dev/ttyACM0', 9600)
+ser_arduino2 = serial.Serial('/dev/ttyUSB0', 115200)
 lamp_states = [0, 0, 0, 0, 0, 0, 0, 0]
+sensor_data_arduino2 = {"gas": 0, "humidity": 0, "temperature": 0, "heatindex": 0, "rain": 0, "soil": 0, "ldr": 0}
 
 @app.route('/')
 def index():
-    return render_template('index.html', lamp_states=lamp_states)
+    return render_template('index.html', lamp_states=lamp_states, sensor_data_arduino2=sensor_data_arduino2)
 
 @app.route('/toggle/<int:lamp_id>')
 def toggle_lamp(lamp_id):
     if 1 <= lamp_id <= 8: 
         lamp_states[lamp_id - 1] = 1 - lamp_states[lamp_id - 1]
         data = {"lamp_id": lamp_id, "state": lamp_states[lamp_id - 1]}
-        ser.write((json.dumps(data) + '\n').encode())
+        ser_arduino1.write((json.dumps(data) + '\n').encode())
     return render_template('index.html', lamp_states=lamp_states)
 
 @app.route('/turn-off-all')
@@ -25,7 +27,7 @@ def turn_off_all():
     for i in range(8):
         lamp_states[i] = 1
         data = {"lamp_id": i + 1, "state": 1}
-        ser.write((json.dumps(data) + '\n').encode())
+        ser_arduino1.write((json.dumps(data) + '\n').encode())
     return render_template('index.html', lamp_states=lamp_states)
 
 @app.route('/turn-on-all')
@@ -33,7 +35,7 @@ def turn_on_all():
     for i in range(8):
         lamp_states[i] = 0
         data = {"lamp_id": i + 1, "state": 0}
-        ser.write((json.dumps(data) + '\n').encode())
+        ser_arduino1.write((json.dumps(data) + '\n').encode())
     return render_template('index.html', lamp_states=lamp_states)
 
 @app.route('/random-toggle')
@@ -42,9 +44,15 @@ def random_toggle():
         if random.random() < 0.5:
             lamp_states[i] = 1 - lamp_states[i]
             data = {"lamp_id": i + 1, "state": lamp_states[i]}
-            ser.write((json.dumps(data) + '\n').encode())
+            ser_arduino1.write((json.dumps(data) + '\n').encode())
     time.sleep(1)
     return render_template('index.html', lamp_states=lamp_states)
+
+@app.route('/update-sensor-data')
+def update_sensor_data():
+    global sensor_data_arduino2
+    sensor_data_arduino2 = json.loads(ser_arduino2.readline().decode())
+    return jsonify(sensor_data_arduino2)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
